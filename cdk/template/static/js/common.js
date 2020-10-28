32734a04160e05c47496eff6494cc114549277b7
zhoulichuan <zhoulichuan@secoo.com>
2020-10-28 19:37:39 +0800

    update js locations

--- cdk/template/static/js/common.js
+++ cdk/template/static/js/common.js
@@ -1,4 +1,4 @@
-document.write('<script type="text/javascript" src="/cdk/static/template/static/js/jquery.placeholder.js"></script>');
-document.write('<script type="text/javascript" src="/cdk/static/template/static/js/jquery.cookie.js?201800506"></script>');
-document.write('<script type="text/javascript" src="/cdk/static/template/static/js/jquery.datetimepicker.js"></script>');
-document.write('<script type="text/javascript" src="/cdk/static/template/static/js/base.js?20190618"></script>');
+document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/barefootedbear/cdn@2.0/cdk/template/static/js/jquery.placeholder.js"></script>');
+document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/barefootedbear/cdn@2.0/cdk/template/static/js/jquery.cookie.js?201800506"></script>');
+document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/barefootedbear/cdn@2.0/cdk/template/static/js/jquery.datetimepicker.js"></script>');
+document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/barefootedbear/cdn@2.0/cdk/template/static/js/base.js?20190618"></script>');
