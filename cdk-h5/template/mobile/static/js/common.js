54ac12e08a2de139de3a3e6f4fabfdf66cb408bc
zhoulichuan <zhoulichuan@secoo.com>
2020-12-04 15:39:19 +0800

    update js path

--- cdk-h5/template/mobile/static/js/common.js
+++ cdk-h5/template/mobile/static/js/common.js
@@ -1,3 +1,3 @@
-document.write('<script src="/cdk_h5/static/template/static/js/jquery.cookie.js?20181116"></script>');
-document.write('<script src="/cdk_h5/static/template/mobile/static/js/touchslide.js"></script>');
-document.write('<script src="/cdk_h5/static/template/mobile/static/js/base.js?201911330"></script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/barefootedbear/cdn@3.0/cdk-h5/template/static/js/jquery.cookie.js?20181116"></script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/barefootedbear/cdn@3.0/cdk-h5/template/mobile/static/js/touchslide.js"></script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/barefootedbear/cdn@3.0/cdk-h5/template/mobile/static/js/base.js?201911330"></script>');
